7a1a21719fda99e930c8e88c3f031b4c5271d829
zane <zanefinnergm@gmail.com>
2022-01-23 01:59:54 -0500

    styled page

--- index.php
+++ index.php
@@ -5,6 +5,16 @@
     <title></title>
     <link rel="stylesheet" href="public/common.css">
   </head>
-  <body>hello world!
+  <body>
+    <div id="nav">
+      <a href="http://localhost:8888">Document</a>
+    </div>
+    <div id="nav-spacer"></div>
+    <div id="body">
+      <h1>I'm a big heading</h1>
+      <h2>I'm a smaller heading</h2>
+      <div class="content">Hello!, I'm content!</div>
+      <div class="content">Hello!, I'm content!</div>
+    </div>
   </body>
 </html>
